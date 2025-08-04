fc7f125bcf63ffd8de095e198f89b5a3d72eea0b
row-huh <rohaabropathan@gmail.com>
2025-08-04 14:29:05 +0500

    nuked everything fuck it

--- app/writing/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getSortedPostsData } from "@/lib/blog"
-import type { BlogPostMetadata } from "@/lib/blog"
-import ClientWritingPage from "./ClientWritingPage"
-
-export default async function WritingPage() {
-  const allBlogPosts: BlogPostMetadata[] = await getSortedPostsData()
-  return <ClientWritingPage initialPosts={allBlogPosts} />
-}
--- app/writing/server.tsx
+++
@@ -1,7 +0,0 @@
-// app/writing/server.tsx
-import { getSortedPostsData } from "@/lib/blog"
-
-export async function getBlogsForClient() {
-  const posts = await getSortedPostsData()
-  return posts
-}
